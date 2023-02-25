8da6f702e2f94a3abd435c36975ff4beee83356d
Marti-code <marti.huza@gmail.com>
2023-02-25 10:21:03 +0100

    attempt 3

--- server/src/index.ts
+++ server/src/index.ts
@@ -27,14 +27,9 @@ app.get("/", (_, res) => {
 
 app.use(bodyParser.json()).use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 // Handle register
